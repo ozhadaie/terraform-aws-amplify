b25aa1894f4e77799f966fb8d98a4daa95989eb0
Oleksii Zhadaiev <ozhadaie@gmail.com>
2022-05-23 12:48:39 +0300

    Refactor outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
-output "cv_dns_record" {
-  value = length(aws_amplify_domain_association.this) == 0 ? "" : aws_amplify_domain_association.this[0].certificate_verification_dns_record
+output "certificate_verification_dns_record" {
+  value       = var.subdomains ? aws_amplify_domain_association.this[0].certificate_verification_dns_record : null
+  description = "The DNS record for certificate verification."
 }
 
-output "sd_dns_record" {
-  value = length(aws_amplify_domain_association.this) == 0 ? [] : aws_amplify_domain_association.this[0].sub_domain[*].dns_record
+output "sub_domain_dns_record" {
+  value       = var.subdomains ? aws_amplify_domain_association.this[0].sub_domain[*].dns_record : null
+  description = "The DNS record for the subdomain."
 }
